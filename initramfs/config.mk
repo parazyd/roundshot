590fdca3288f3aa071dea823fa57a4ae4b4c622a
parazyd <parazyd@dyne.org>
2017-12-15 18:43:22 +0100

    Drop dropbear, change gpgv-static url.

--- initramfs/config.mk
+++ initramfs/config.mk
@@ -4,13 +4,9 @@ BUSYBOX = 1
 BUSYBOX_VERSION = 1.27.2
 BUSYBOX_URL = https://busybox.net/downloads/busybox-$(BUSYBOX_VERSION).tar.bz2
 
-DROPBEAR = 0
-DROPBEAR_VERSION = 2017.75
-DROPBEAR_URL = https://matt.ucc.asn.au/dropbear/releases/dropbear-$(DROPBEAR_VERSION).tar.bz2
-
 GNUPG = 1
 GNUPG_VERSION = 2.2.3-1
-GNUPG_URL = http://ftp.debian.org/debian/pool/main/g/gnupg2/gpgv-static_$(GNUPG_VERSION)_armhf.deb
+GNUPG_URL = https://deb.debian.org/debian/pool/main/g/gnupg2/gpgv-static_$(GNUPG_VERSION)_armhf.deb
 
 TC_PATH = $(PWD)/../toolchain/arm-linux-musleabihf
 TC_NAME = arm-linux-musleabihf-
