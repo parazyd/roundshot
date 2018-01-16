fd974875ed6f21b5d1eb97df913efdd65b798dc7
parazyd <parazyd@dyne.org>
2018-01-16 14:05:51 +0100

    Bump gpgv version.

--- initramfs/config.mk
+++ initramfs/config.mk
@@ -5,7 +5,7 @@ BUSYBOX_VERSION = 1.27.2
 BUSYBOX_URL = https://busybox.net/downloads/busybox-$(BUSYBOX_VERSION).tar.bz2
 
 GNUPG = 1
-GNUPG_VERSION = 2.2.3-1
+GNUPG_VERSION = 2.2.4-1
 GNUPG_URL = https://deb.debian.org/debian/pool/main/g/gnupg2/gpgv-static_$(GNUPG_VERSION)_armhf.deb
 
 TC_PATH = $(PWD)/../toolchain/arm-linux-musleabihf
